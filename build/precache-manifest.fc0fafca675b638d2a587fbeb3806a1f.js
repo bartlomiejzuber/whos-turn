2ea380c33163cd74c424cea571ba4ae4cbb6227b
Bajtas <bajtas@gmail.com>
2018-11-08 10:50:14 +0100

    insert built app for demo purposes

---
+++ build/precache-manifest.fc0fafca675b638d2a587fbeb3806a1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "79fcea166611765a874d",
+    "url": "/static/js/main.79fcea16.chunk.js"
+  },
+  {
+    "revision": "58141f363566f2b1b875",
+    "url": "/static/js/1.58141f36.chunk.js"
+  },
+  {
+    "revision": "79fcea166611765a874d",
+    "url": "/static/css/main.5ae827d7.chunk.css"
+  },
+  {
+    "revision": "25eef7f817707a1c88f54fe35b2655f8",
+    "url": "/index.html"
+  }
+];
